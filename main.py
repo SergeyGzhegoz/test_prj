1ab91dad23ec60df0cb5c6f3a8ab3e829ce56fe6
SergeyGzhegoz <s.gz@yandex.ru>
2022-11-25 20:53:59 +0300

    Написали метод area

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 class Rectangle:
     def __init__(self, a, b):
-        self.a = a
-        self.b = b
+        self._a = a
+        self._b = b
+
+    def area(self):
+        return self._a * self._b
 
 
 main_rect = Rectangle(5, 4)
+print(main_rect.area())
