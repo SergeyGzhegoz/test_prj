c04067aa0fdd5cba8095eecc2ff957663be1d311
SergeyGzhegoz <s.gz@yandex.ru>
2022-11-25 21:58:34 +0300

    Написали методы get

--- main.py
+++ main.py
@@ -8,10 +8,19 @@ class Rectangle:
 
     def get_height(self):
         return self._a
-    
+
     def get_width(self):
         return self._b
 
-    
+    def set_height(self, a):
+        self._a = a
+
+    def set_width(self, b):
+        self._b = b
+
+
 main_rect = Rectangle(5, 4)
+print(main_rect.area())
+main_rect.set_height(7)
+main_rect.set_width(8)
 print(main_rect.area())
